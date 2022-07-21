454bb3aa2d299e33d8272e5b0c222f8489d15eb4
SlawaSlawaSlawa <100555254+SlawaSlawaSlawa@users.noreply.github.com>
2022-07-21 20:55:55 +0400

    day three

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 import menu from "./modules/menu.js";
 import modal from "./modules/modal.js";
+import tabsAboutUs from "./modules/tabs-about-us.js";
+import exchangeRates from "./modules/exchange-rates.js";
 
 menu()
 modal()
+tabsAboutUs()
+exchangeRates()
